c792d64bbf13ea291e3898eeda18a2459d3064d8
rifahammet <58286411+rifahammet@users.noreply.github.com>
2019-11-28 21:05:36 +0700

    Update authorize.php

--- app/Views/oauth2/authorize.php
+++ app/Views/oauth2/authorize.php
@@ -6,13 +6,12 @@ $action = "/oauth2/Authorize?response_type=".$response_type."&client_id=".$clien
 <div style="text-align:center;">
  	<form method="post">
       	<div class="oauth_content" node-type="commonlogin">
-        	<p class="oauth_main_info"> 授权  <a href="http://homeway.me"  target="_blank" class="app_name">homeway 云计算平台</a> 访问你的xxx帐号，并同时登录</p>
+        	<p class="oauth_main_info"><a href="http://satualgoritma.com"  target="_blank" class="app_name">The One Algo</a></p>
 			<input name="authorized" value="yes" hidden>
-			<button>submit</button>
-			<!--这里做登陆检查：未登录要求登陆，已登陆直接授权-->
+			<button>Submit</button>
 		</div>
     </form>
-    <p>提示：为保障帐号安全，请认准本页URL地址必须以 https://homeway.me 开头</p>
+    <p><b>Press Button Submit</b></p>
     	
    	</div>
 </body>
